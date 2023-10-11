9b966180bbe7f971686bdb5c0726688478c6a01e
Kritan Dotel <dotelkritan@gmail.com>
2023-10-11 07:57:41 +0545

    react-proj

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import RootLayOut from './components/RootLayOut'
+import NotFound from './pages/NotFound'
+import HomePage from './pages/HomePage'
+
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<RootLayOut />} >
+        <Route index element={<HomePage />} />
+
+        <Route path='*' element={<NotFound />} />
+
+      </Route>
+
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Crud from '../components/Crud'
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <Crud />
+
+
+    </div>
+  )
+}
+
+export default HomePage
