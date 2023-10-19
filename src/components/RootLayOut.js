d77c00e3620b91249844dde5281380b5dccde7ee
Kritan Dotel <dotelkritan@gmail.com>
2023-10-19 15:46:51 +0545

    changes

--- src/components/RootLayOut.js
+++ src/components/RootLayOut.js
@@ -2,11 +2,16 @@ import React from 'react'
 import { Outlet } from 'react-router'
 import Header from './Header'
 
+
+
 const RootLayOut = () => {
   return (
     <>
       <Header />
       <Outlet />
+
+
+
 
 
 
